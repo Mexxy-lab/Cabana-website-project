cc8e1f70bba88dd29ffb6f11352bcf1d2a8d2eb3
Emeka Umejiofor <pumej1985@gmail.com>
2023-10-30 02:21:48 -0400

    updated app.py file

--- website/apps.py
+++ website/apps.py
@@ -4,3 +4,6 @@ from django.apps import AppConfig
 class WebsiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'website'
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=3000)
