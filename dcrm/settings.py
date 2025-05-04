eb41318af0ad25ad5add381c4ca0d9c6732f64c4
it-admins <it-admins@ectios.com>
2025-05-04 02:40:25 -0400

    Ignore __pycache__, venv, and local dev files

--- dcrm/settings.py
+++ dcrm/settings.py
@@ -83,7 +83,7 @@ DATABASES = {
         'PASSWORD' : 'mekulus',
         'HOST': '172.17.0.2',
         'PORT': '3306',
-        # 'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3',
         # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
